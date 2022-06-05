41d253ef5e61de5636440c31ae3fd528b30f7b7e
Ilya <106909360+IlyaUlskiy@users.noreply.github.com>
2022-06-05 17:50:35 +0700

    Update second_win.py

--- second_win.py
+++ second_win.py
@@ -1,1 +1,7 @@
-
+from PyQt5.QtCore import Qt, Qtimer, QTime, QLocale
+from PyQt5.QtGui import QDoubleValidator, QIntValidator, QFont 
+from PyQt5.QtWidgets import (
+  QApplication, Qwidget,
+  QHBoxLayout, QVBoxLayout, QGridLayout,
+  QGroupBox, QRadioButton,
+  QPushButton, QLabel, QListWidget, QLineEdit)
